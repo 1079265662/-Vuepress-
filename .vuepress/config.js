f73b829f2a626b724cbaf02e21caf76ed79ab49f
刘凯利 <qq1079265662@163.com>
2021-05-20 09:53:56 +0800

    完善导航栏

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -52,7 +52,7 @@ module.exports = {
         text: '关于本播客',
         icon: 'reco-other',
         items: [
-          { text: '关于我', link: '/other/', icon: 'reco-message' },
+          { text: '关于我', link: '/other/', icon: 'reco-account' },
           { text: '联系我', link: '/about/', icon: 'reco-other' }
         ]
       },
